71e9e01008519a7976907f49441d9521a9867e2e
souravdutta8012 <souravdutta2108@gmail.com>
2022-09-06 23:42:43 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import { AppBar, Box, Toolbar, Typography } from '@mui/material';
 import './App.css';
 
 function App() {
   return (
     <>
-      hi
+      <AppBar position="sticky">
+        <Toolbar>
+        </Toolbar>
+      </AppBar>
+      <Box className='flex'>
+        <Box className='w-1/2'>
+          <Typography>
+            Write your text
+          </Typography>
+        </Box>
+        <Box className='w-1/2'>
+          <Typography>
+            Hieroglyphs
+          </Typography>
+        </Box>
+      </Box>
     </>
   );
 }
