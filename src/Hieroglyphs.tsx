4be905052114d30209a24728efb99d2a8a8e470e
souravdutta8012 <souravdutta2108@gmail.com>
2022-09-20 22:41:17 +0530

    changes

--- src/Hieroglyphs.tsx
+++ src/Hieroglyphs.tsx
@@ -1,4 +1,4 @@
-import { Box, TextField, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 export default function Hieroglyphs(props: any) {
     const { value } = props;
